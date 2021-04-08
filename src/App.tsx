8f33d84e08676c9f5da2bdd15e389a80df05fecf
Hamza Mehri <azizelmehri@gmail.com>
2021-04-08 13:29:59 +0100

    installed marterial ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
